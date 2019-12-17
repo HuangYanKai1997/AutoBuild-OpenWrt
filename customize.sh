487ba340aee7fecef70464249f9e5d7a39ecb600
Drageye <35104751+HuangYanKai1997@users.noreply.github.com>
2019-12-17 16:19:19 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -7,3 +7,10 @@
 #=================================================
 #1. Modify default IP
 sed -i 's/192.168.3.18/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
+git clone https://github.com/pymumu/luci-app-smartdns.git
+git clone https://github.com/maxlicheng/luci-app-unblockmusic.git
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git
+git clone https://github.com/apollo-ng/luci-theme-darkmatter.git
+git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git
+git clone https://github.com/themeskult/lucid-theme.git
+git clone https://github.com/Aslin-Ameng/luci-theme-Night.git
